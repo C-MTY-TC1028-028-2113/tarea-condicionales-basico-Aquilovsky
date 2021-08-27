9d27b2f0fd3cd24ec785a173939b70114b2f9734
Aquilovsky <a01284966@itesm.mx>
2021-08-26 23:08:30 -0500

    Listo

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -2,6 +2,18 @@
 def main():
     edad = int(input("Ingresa tu edad: "))
     #Aquí empieza tu programa...
+    if(edad >= 18):
+        id = str(input("¿Tienes identificación oficial? (s/n): "))
+        if (id == 's'):
+            print('Trámite de licencia concedido')
+        elif (id == 'n'): 
+            print('No cumples requisitos')
+        else:
+            print('Respuesta incorrecta')
+    elif(0< edad <18):
+        print('No cumples requisitos')
+    else:
+        print('Respuesta incorrecta')
 
 if __name__ == '__main__':
     main()
